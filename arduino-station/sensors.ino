6e0cfd912ee65b876b12f5a9d1246f344fd0d757
Adam Wałach <adam.walach@sap.com>
2017-04-06 09:59:09 +0200

    add automatic uuid generation

--- arduino-station/sensors.ino
+++ arduino-station/sensors.ino
@@ -1,5 +1,4 @@
 void initializeSensors() {
-
   initializeBME();
   initializePMS();
   timer.every(READ_CYCLE, readAndSend);
@@ -11,12 +10,11 @@ void readAndSend() {
   PmsData pmsData = pmsLoop();
   data.pm25 = pmsData.pm25;
   data.pm10 = pmsData.pm10;
-  Measurement measurement = {"1", {"ABC1234", "0.0.1", "BME280"}, data};
-  
+  Measurement measurement = {"1", {"uuid", "0.0.1", "BME280"}, data};
+  memcpy( measurement.id.uuid, ctx.deviceID, UUID_LENGTH*sizeof(char));
   bool msrSent = sendMeasurement(&measurement, sizeof(measurement));
   
   if (!msrSent) {
-  
     Serial.println("Could not sent measurement");
   }
 }
