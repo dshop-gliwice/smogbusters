c5ffcd678aae34ab0bbbb8fc18e6887e1fc9fbaa
Łukasz Socha <drjolo@gmail.com>
2017-04-13 13:20:22 +0200

    DS1820 support - pin D7

--- arduino-station/sensors.ino
+++ arduino-station/sensors.ino
@@ -1,6 +1,11 @@
+#define DS1820_INITDELAY 100
+
 void initializeSensors() {
   initializeBME();
   initializePMS();
+  if(initializeDS()) {
+    timer.after(DS1820_INITDELAY, requestDS1820);
+  }
   timer.every(READ_CYCLE, readAndSend);
 }
 
@@ -11,6 +16,7 @@ void readAndSend() {
   data.pm25 = pmsData.pm25;
   data.pm10 = pmsData.pm10;
   data.pm1 = pmsData.pm1;
+  data.DStemp = DStemp;
   Measurement measurement = {"1", {"uuid", "0.0.1", "BME280"}, data};
   memcpy( measurement.id.uuid, ctx.deviceID, UUID_LENGTH*sizeof(char));
   bool msrSent = sendMeasurement(&measurement, sizeof(measurement));
