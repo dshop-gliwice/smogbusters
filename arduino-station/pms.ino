62be39cef33eacc259167b8b6c7a53b101d42f90
Adam Wałach <adam.walach@gmail.com>
2018-10-03 10:46:50 +0200

    Add heaterVoltage to eeprom

--- arduino-station/pms.ino
+++ arduino-station/pms.ino
@@ -4,9 +4,6 @@ SoftwareSerial swSer(14, 12, false, 256);
 PMS pms(swSer);
 PMS::DATA rawData;
 
-//#define LENG 31   //0x42 + 31 bytes equal to 32 bytes
-//unsigned char buf[LENG];
-
 int PM01Value = 0;        //define PM1.0 value of the air detector module
 int PM2_5Value = 0;       //define PM2.5 value of the air detector module
 int PM10Value = 0;       //define PM10 value of the air detector module
